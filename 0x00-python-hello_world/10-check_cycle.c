e6600da9246a1253cc14f9163858903fa7a069af
gnnrbachelor <gnnrbachelor@gmail.com>
2021-04-26 19:33:51 +0000

    Add 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks for cycle
+ *
+ * @list: Linked list
+ *
+ * Return: 0 if no cycle, 1 if cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = NULL;
+	listint_t *fast = NULL;
+
+	if (!list)
+		return (0);
+
+	slow = list->next;
+	fast = list->next->next;
+
+	while (slow && fast && fast->next)
+	{
+		if (slow == fast)
+			return (1);
+
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return(0);
+}
