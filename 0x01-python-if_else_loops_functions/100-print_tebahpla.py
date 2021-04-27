25cd8acfa87b3f3a0088cccb1c0bef8ef797676e
gnnrbachelor <gnnrbachelor@gmail.com>
2021-04-27 23:25:35 +0000

    Fix 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
-alpha = []
-
 for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2 == 0:
-        alpha.append(chr(i))
+        print("{}".format(chr(i)), end="")
     else:
-        alpha.append(chr(i - 32))
-for i in range(len(alpha)):
-    print("{}".format(alpha[i]), end="")
+        print("{}".format(chr(i - 32)), end="")
