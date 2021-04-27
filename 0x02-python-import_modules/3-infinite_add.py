56a870f84787e5d594a71b4545b8e310002c01a6
gnnrbachelor <gnnrbachelor@gmail.com>
2021-04-27 22:08:46 +0000

    pep8 for 3-infinite_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     sum = 0
     num_arg = len(argv) - 1
     for i in range(num_arg):
-       sum += int(argv[i + 1])
+        sum += int(argv[i + 1])
     print("{}".format(sum))
