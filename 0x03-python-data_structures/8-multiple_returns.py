085ae30d9222c82f0c49161adfb80205b26ddd11
gnnrbachelor <gnnrbachelor@gmail.com>
2021-04-28 20:03:31 +0000

    Add 8-multiple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    strlen = len(sentence)
+    if strlen == 0:
+        c = None
+    else:
+        sen_tup = (strlen, sentence[0])
+    return (sen_tup)
