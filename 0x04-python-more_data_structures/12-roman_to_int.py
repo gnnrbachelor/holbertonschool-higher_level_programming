e1d9cce19b2bc05e8bd16aa52a7a18d180c99328
gnnrbachelor <gnnrbachelor@gmail.com>
2021-04-29 23:08:30 +0000

    Add 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+
+    sum = 0
+    prev = 0
+    for i in range(len(roman_string)):
+        if dict.get(roman_string[i], 0) == 0:
+            return (0)
+
+        current = dict.get(roman_string[i])
+
+        if current > prev:
+            sum += current - 2 * prev
+        else:
+            sum += current
+
+        prev += current
+
+    return (sum)
