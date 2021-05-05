e06061ca2b9d71400d98d796f195fc59b09d019e
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-05 18:11:39 +0000

    Add 4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 4-print_square """
+
+def print_square(size):
+    """prints square"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
