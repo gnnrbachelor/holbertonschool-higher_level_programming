d5f699f30bed1ac6cd595cbd8fc35b8a66e6c20a
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-07 00:35:37 +0000

    Fix 5-text_indent

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""5-text_indentation module. Adds 2 lines after . ? and :"""
-
+"""5-text_indentation module"""
 
 def text_indentation(text):
-    """ Adds two lines after special characters"""
+    """Adds two lines after special characters """
     if type(text) is not str:
         raise TypeError("text must be a string")
-
-    string = ""
-    spec_char = ['.', '?', ':']
+    flag = False
     for i in text:
-        string += i
-        if i in spec_char:
-            string += "\n\n"
-    print(string, end="")
+        if flag:
+            if i == " ":
+                continue
+            flag = False
+        if i == '.' or i == '?' or i == ':':
+            print(i)
+            print("")
+            flag = True
+        else:
+            print(i, end="")
