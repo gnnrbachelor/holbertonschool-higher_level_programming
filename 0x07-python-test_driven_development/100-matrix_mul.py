5f5ba69aa862c6f9f7e0403d7150752d09b91484
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-10 20:28:35 +0000

    Fixing 100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -11,10 +11,10 @@ def matrix_mul(m_a, m_b):
             raise TypeError("m_a must be a list of lists")
 
     if type(m_b) is not list:
-        raise TypeError("m_a must be a list")
+        raise TypeError("m_b must be a list")
     for i in m_b:
         if type(i) is not list:
-            raise TypeError("m_a must be a list of lists")
+            raise TypeError("m_b must be a list of lists")
 
     if len(m_a) == 0:
         raise ValueError("m_a can't be empty")
