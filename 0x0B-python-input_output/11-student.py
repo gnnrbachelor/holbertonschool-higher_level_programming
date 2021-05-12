e327ea76f07ae336b0a093526f58fe69f429e9f7
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-12 22:40:08 +0000

    Add 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module for class Student"""
+
+
+class Student:
+    """Class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializer method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dict rep"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            student_dict = {}
+            for key in attrs:
+                try:
+                    student_dict[key] = self.__dict__[key]
+                except KeyError:
+                    continue
+            return student_dict
+
+    def reload_from_json(self, json):
+        """Replace all attributes"""
+        for key in json.keys():
+            self.__dict__[key] = json[key]
