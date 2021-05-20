59fd8c3a5a9abf0fa3dc8c1a95a54609e427c3d2
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-20 20:37:10 +0000

    Add 17-main.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,30 @@ class Base:
             return []
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes JSON string rep of list_objs"""
+        with open(cls.__name__+'.json', 'w+') as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                    dict_list = [i.to_dictionary() for i in list_objs]
+                    file.write(Base.to_json_string(dict_list))
+
+    def from_json_string(json_string):
+        """Returns list from JSON"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns instances with all attributes"""
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                temp = cls(1, 1)
+            elif cls.__name__ == "Square":
+                temp = cls(1)
+            temp.update(**dictionary)
+            return temp
