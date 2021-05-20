510552dd4bbd6429cefe3d20d5814fd472c46da4
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-20 12:15:39 +0000

    Add 7-main.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,18 @@ class Rectangle(Base):
     def __str__(self):
         """__str__ method"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args):
+        """Update rectangle"""
+        arg_len = len(args)
+        if arg_len != 0:
+            if arg_len >= 1:
+                self.id = args[0]
+            if arg_len >= 2:
+                self.width = args[1]
+            if arg_len >= 3:
+                self.height = args[2]
+            if arg_len >= 4:
+                self.x = args[3]
+            if arg_len >= 5:
+                self.y = args[4]
