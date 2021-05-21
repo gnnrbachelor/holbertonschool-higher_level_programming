0cde1a411410ec0d3b61b61802b006f8514c10ca
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-21 23:48:40 +0000

    Add tests for square

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for testing Square"""
+
+
+import unittest
+import pep8
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class Test_Square_Inst(unittest.TestCase):
+    """Unittests for Square Class"""
+
+    def test_base_parent(self):
+        """Test for Base class as parent"""
+        self.assertIsInstance(Square(1), Base)
+
+    def test_two_arguents(self):
+        """Test for two arguments"""
+        square_1 = Square(1, 2)
+        square_2 = Square(1, 2)
+        self.assertEqual(square_1.id, square_2.id - 1)
