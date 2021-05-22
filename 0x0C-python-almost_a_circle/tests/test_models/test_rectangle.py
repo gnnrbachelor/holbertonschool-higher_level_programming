4bd2c0ce9da85268b1a292a89f1749b821399590
gnnrbachelor <gnnrbachelor@gmail.com>
2021-05-22 02:19:16 +0000

    Add tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -35,6 +35,10 @@ class Test_Rect_Inst(unittest.TestCase):
         rect_2 = Rectangle(1, 2, 3, 4)
         self.assertEqual(rect_1.id, rect_2.id - 1)
 
+    def test_rect_five_arguments(self):
+        """Test Five arguments"""
+        self.assertEqual(5, Rectangle(1, 2, 3, 4, 5).id)
+
     def test_rect_six_args(self):
         """Tests six arguments"""
         with self.assertRaises(TypeError):
