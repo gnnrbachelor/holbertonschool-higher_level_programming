6f49843dcba52df600c30c1ca8f89e2487670b5f
gnnrbachelor <gnnrbachelor@gmail.com>
2021-08-02 23:16:44 +0000

    Add to 13-add.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-let length = process.argv.length;
+const length = process.argv.length;
 
 if (length <= 3) {
-	console.log(0);
+  console.log(0);
 } else {
-let argList = process.argv.map(Number).sort((a, b) => (a - b));
-console.log(argList[length - 2]);
+  const argList = process.argv.map(Number).sort((a, b) => (a - b));
+  console.log(argList[length - 2]);
 }
-
