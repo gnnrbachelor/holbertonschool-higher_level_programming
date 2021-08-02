bd1666d9b914d2ad45d0654d19f2d0be5ddc3a68
gnnrbachelor <gnnrbachelor@gmail.com>
2021-08-02 21:18:58 +0000

    Fix semi

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 
 const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
-	console.log('Missing number of occurences');
+  console.log('Missing number of occurences');
 } else {
-	for (let i = x; i > 0; i--) {
-		console.log('C is fun');
-	}
+  for (let i = x; i > 0; i--) {
+    console.log('C is fun');
+  }
 }
