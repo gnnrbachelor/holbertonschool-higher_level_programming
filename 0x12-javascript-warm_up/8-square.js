e28009e47d6b4cb4605bdb0782e855f250eebcd6
gnnrbachelor <gnnrbachelor@gmail.com>
2021-08-02 21:51:06 +0000

    Fix semi

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 
-let size = Math.floor(process.argv[2]);
+const size = Math.floor(process.argv[2]);
 if (isNaN(size)) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else {
-	for (i = 0; i < size; i++) {
-		for (j = 0; j < size; j++) {
-			process.stdout.write('X');
-		}
-		if (j === size) {
-			console.log();
-		}
-	}
+  for (let i = 0; i < size; i++) {
+    let k = 0;
+    for (let j = 0; j < size; j++) {
+      process.stdout.write('X');
+      k++;
+    }
+    if (k === size) {
+      console.log();
+    }
+  }
 }
-
