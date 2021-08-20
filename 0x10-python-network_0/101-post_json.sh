42c4cf68aabd7e085e500bc3e827478bf637a907
root <gnnrbachelor@gmail.com>
2021-08-20 14:30:40 +0000

    Fix 101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends JSON post request
-curl -sX "$1" POST -H "Content-Type: application/json" -d "$(cat "$2")"
+curl -s "$1" -X POST -H "Content-Type: application/json" -d "$(cat "$2")"
