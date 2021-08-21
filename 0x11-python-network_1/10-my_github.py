5ba4a45d2f2a20af33144ca17d25225b58894019
typh0n1ankill <typh0n1ankill@typh0n1ankills-MacBook-Air.local>
2021-08-20 20:30:19 -0500

    Fix github auth

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,6 +3,9 @@
 if __name__ == "__main__":
     import requests
     from sys import argv
-    url = "https://api.github.com/users"
+    url = "https://api.github.com/user"
     response = requests.get(url, auth=(argv[1], argv[2])).json()
-    print(response.get('id'))
+    if "id" in response:
+        print(response['id'])
+    else:
+        print(None)
