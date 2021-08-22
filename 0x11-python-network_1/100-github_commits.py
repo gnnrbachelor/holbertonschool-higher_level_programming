174a1152bc3fce0903dd67853398fe251e17ab03
typh0n1ankill <typh0n1ankill@typh0n1ankills-MacBook-Air.local>
2021-08-22 08:44:55 -0500

    Simplify 100-github

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,15 +4,11 @@ from sys import argv
 import requests
 
 if __name__ == "__main__":
-    """
-    Returns github data
-    """
-    req = requests.get("https://api.github.com/repos/{}/{}/commmits"
-                       .format(argv[2], argv[1])).json()
-    if len(req) > 10:
+    req = requests.get("https://api.github.com/repos/{}/{}/commmits".format(argv[2], argv[1]))
+    data = req.json()
+    if len(data) > 10:
         size = 10
     else:
-        size = len(req)
+        size = len(data)
     for element in range(size):
-        print("{}: {}".format(req[element].get(
-             'sha'), req[element].get('commit').get('author').get('name')))
+        print("{}: {}".format(data[element].get('sha'), data[element].get('commit').get('author').get('name')))
