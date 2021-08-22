0e81f7fc32e5ebb7c6522bed398335cfba4743e0
typh0n1ankill <typh0n1ankill@typh0n1ankills-MacBook-Air.local>
2021-08-22 08:01:58 -0500

    Fixing comment

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-""" Returns github data """
+"""Returns github data"""
 from sys import argv
 import requests
 
 if __name__ == "__main__":
-    """Returns github data"""
+    """
+    Returns github data
+    """
     req = requests.get("https://api.github.com/repos/{}/{}/commmits"
                        .format(argv[2]. arv[1])).json()
-if len(req) > 10:
-    size = 10
-else:
-    size = len(req)
-for element in range(size):
-    print("{}:{}".format(req[element].get(
-         'sha'), req.get('commit').get('author').get('name')))
+    if len(req) > 10:
+        size = 10
+    else:
+        size = len(req)
+    for element in range(size):
+        print("{}:{}".format(req[element].get(
+            'sha'), req.get('commit').get('author').get('name')))
