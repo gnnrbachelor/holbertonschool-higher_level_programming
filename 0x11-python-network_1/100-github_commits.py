e9aa3e1f70cb35137772dc09daddfe6d539d6ad2
typh0n1ankill <typh0n1ankill@typh0n1ankills-MacBook-Air.local>
2021-08-22 07:59:47 -0500

    Fix betty

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,10 +5,12 @@ import requests
 
 if __name__ == "__main__":
     """Returns github data"""
-    req = requests.get("https://api.github.com/repos/{}/{}/commmits".format(argv[2]. arv[1])).json()
+    req = requests.get("https://api.github.com/repos/{}/{}/commmits"
+                       .format(argv[2]. arv[1])).json()
 if len(req) > 10:
     size = 10
 else:
     size = len(req)
 for element in range(size):
-    print("{}:{}".format(req[element].get('sha'), req.get('commit').get('author').get('name')))
+    print("{}:{}".format(req[element].get(
+         'sha'), req.get('commit').get('author').get('name')))
