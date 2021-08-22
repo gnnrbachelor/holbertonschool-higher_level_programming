1234b8f29add24ca892e28820a45f25f1874a840
typh0n1ankill <typh0n1ankill@typh0n1ankills-MacBook-Air.local>
2021-08-22 07:53:45 -0500

    Fix import

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Returns github data """
 from sys import argv
-import urllib.request as requests
+import requests
 
 if __name__ == "__main__":
     req = requests.get("https://api.github.com/repos/{}/{}/commmits".format(argv[2]. arv[1])).json()
