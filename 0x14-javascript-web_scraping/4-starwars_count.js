1484f3697cd70be28c4457ef88a36a1381046ed7
root <gnnrbachelor@gmail.com>
2021-09-14 19:24:21 +0000

    Add 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,19 +10,19 @@ request(url, (err, response, body) => {
   if (err != null) {
     console.log(err);
   } else {
-	payload = JSON.parse(body).results;
-	
-	for (let i = 0; i < payload.length; i++) {
-		let char_list = payload[i].characters;
-		for (let j = 0; j < char_list.length; j++) {
-			let character = char_list[j];
-			let id = character.split('/')[5];
-			
-			if (id === '18') {
-				count += 1;
-			}
-		}
+    const payload = JSON.parse(body).results;
+
+    for (let i = 0; i < payload.length; i++) {
+      const charList = payload[i].characters;
+      for (let j = 0; j < charList.length; j++) {
+        const character = charList[j];
+        const id = character.split('/')[5];
+
+        if (id === '18') {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
   }
-	console.log(count);
-}
 });
